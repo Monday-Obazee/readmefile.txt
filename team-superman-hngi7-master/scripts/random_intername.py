f3bc7490cbdb1ebb92c45df7fa8aadf678140b33
Monday-Obazee <Mondayobazee@fgmail.com>
2020-06-04 14:33:38 +0100

    added my task

---
+++ team-superman-hngi7-master/scripts/random_intername.py
@@ -0,0 +1,12 @@
+Name = "Odinakachi Nnuforole"
+ID = "HNG-00938"
+Language = "Python"
+Email = "nnuforole.odinaka@gmail.com"
+
+def greeting():
+    print("Hello World, this is" , Name
+           +" with HNGi7" + " ID", ID
+          +" using" , Language + " for stage 2 task."
+          +Email)
+
+greeting()
